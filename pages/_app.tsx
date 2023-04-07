00f058be3ab66d76b77405e1c61ca7ac00f8bf0a
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-07 15:38:38 +0100

    so far

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../globalstyles'
+
+const theme: DefaultTheme = {
+  primary:"#040714",
+  secondary: "#0070f3",
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+  
+  <ThemeProvider theme={theme}>
+    <GlobalStyle  />
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
