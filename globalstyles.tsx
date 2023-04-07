4b254f8fea609c8bb8e0b6c368cc060ccf8f0d07
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-07 15:59:38 +0100

    styled d file

--- globalstyles.tsx
+++ globalstyles.tsx
@@ -1,14 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
+
+
+
+
 const GlobalStyle = createGlobalStyle`
 
   html,
   body {
-    color: ${({ theme }) => theme.primary};
+    color: ${({theme}) =>theme.primary}
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
   a {
@@ -16,14 +20,9 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
-  h1, h2, h3, h4 {
-    color: ${({ theme }) => theme.white};
-    margin-bottom: 0;
-  }
 
-  p {
-    color: ${({ theme }) => theme.silver};
-  }
+
+ 
   * {
     box-sizing: border-box;
   }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
 import GlobalStyle from '../globalstyles'
 
-const theme: DefaultTheme = {
-  primary:"#040714",
-  secondary: "#0070f3",
-};
+ export const theme: DefaultTheme = {
+   primary: "#040714",
+   secondary: "#0070f3",
+   white: '',
+   brightBlue: '',
+   silver: '',
+   darkGrey: ''
+ };
 
 function MyApp({ Component, pageProps }: AppProps) {
 
