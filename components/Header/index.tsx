da85490c227945203dd99529b93306e6b6d347e1
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-08 17:23:49 +0100

    logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Nav } from './style'
+import { Logo, Nav } from './style'
 
 
 
 function Header() {
   return (
-    <Nav>Header</Nav>
+    <Nav><Logo src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original"
+    alt="Disney Logo" ></Logo> </Nav>
   )
 }
 
