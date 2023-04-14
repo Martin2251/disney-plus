1156896d671a2de8d671da05e3fe03b0691ba736
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-14 07:23:08 +0100

    signup

---
+++ pages/login.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import SignUp from '../components/SignUp'
+
+
+const Login: NextPage = () => {
+  return (
+   <>
+   <SignUp  />
+   
+   </>
+
+
+
+  
+  )
+}
+
+export default Login
