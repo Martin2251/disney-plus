5d8eded65e380628ba684716552e769908f4d27c
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-14 07:42:43 +0100

    amended global styles

--- globalstyles.tsx
+++ globalstyles.tsx
@@ -1,18 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
-
-
-
-
 const GlobalStyle = createGlobalStyle`
-
   html,
   body {
-    color: ${({theme}) =>theme.primary}
+    color: ${({ theme }) => theme.primary};
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
   a {
@@ -20,9 +15,14 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
+  h1, h2, h3, h4 {
+    color: ${({ theme }) => theme.white};
+    margin-bottom: 0;
+  }
 
-
- 
+  p {
+    color: ${({ theme }) => theme.silver};
+  }
   * {
     box-sizing: border-box;
   }
