c63440cad7d74f66b3c3f12c6ab5689778462bd9
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-16 19:21:56 +0100

    react slick

---
+++ components/HomeLayout/SliderBanner/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+
+function SliderBanner () {
+  return (
+    <div>SliderBanner </div>
+  )
+}
+
+export default SliderBanner 
