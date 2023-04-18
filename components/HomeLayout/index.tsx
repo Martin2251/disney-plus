9524b5666aa52fa47ccc30024064d407dc63d6c7
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-18 07:51:51 +0100

    catogries

--- components/HomeLayout/index.tsx
+++ components/HomeLayout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Categories from './Categories'
 
 import SliderBanner from './SliderBanner'
 import { Container } from './styles'
@@ -9,6 +10,7 @@ type Props = {}
   return (
    <Container>
           <SliderBanner  />
+          <Categories  />
     </Container >
       
 
